426234d976f2e8766efbbef73735a0b6339666cb
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-08-12 02:19:10 +0200

    ✨ F02: APP > Add routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./Components/pages/login/LoginPages";
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        {/* <Route path="/order" element={<OrderPage />} /> */}
+        {/* <Route path="*" element={<ErrorPage />} /> */}
+      </Routes>
     </>
   );
 }
