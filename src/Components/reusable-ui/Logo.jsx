d2e33032e2cc664bb7530c6dca2b0fdeebe1a923
Titouan Moussiaux <moussiaux.titouan@gmail.com>
2023-08-26 16:55:16 +0200

    💄 F03: LOGO > Add styling

--- src/Components/reusable-ui/Logo.jsx
+++ src/Components/reusable-ui/Logo.jsx
@@ -1,11 +1,33 @@
 import { styled } from "styled-components";
+import logo from "../../assets/images/logo-orange.png";
+import {theme} from '../../theme/index'
 
 export default function Logo() {
   return (
-    <LogoStyled>Logo Crazee Burger</LogoStyled>
+    <LogoStyled>
+      <h1>Crazee</h1>
+      <img src={logo} alt="Crazee Burger Logo" />
+      <h1>Burger</h1>
+      </LogoStyled>
   )
 }
 
 const LogoStyled = styled.div`
-  background-color: green;
+  display: flex;
+  align-items: center;
+
+  h1 {
+    color: ${theme.colors.primary};
+    font-family: 'Amatic SC', cursive;
+    font-weight: ${theme.weights.bold};
+    font-size: 110px;
+    text-transform: uppercase;
+    letter-spacing: 1.5px;
+  }
+  img {
+    object-fit: contain;
+    object-position: center;
+    height: 150px;
+    margin: 0 5px;
+  }
 `;
