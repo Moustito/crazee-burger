2d4fcaf9c05a8289d6d0f3c0b7f96dafe5f6df24
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-10-11 14:15:45 +0200

    💄 F04: ORDER > Main > Add styles

--- src/Components/pages/order/Main.jsx
+++ src/Components/pages/order/Main.jsx
@@ -1,10 +1,12 @@
 import { styled } from "styled-components";
 
 export default function Main() {
-  return <MainStyled>Main</MainStyled>;
+  return <MainStyled></MainStyled>;
 }
 
 const MainStyled = styled.div`
-  background: green;
   flex: 1;
+  border-radius: 0px 0px 15px 15px;
+  background: #f5f5f7;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 `;
