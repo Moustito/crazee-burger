a1fbc74228e2c8c66a1ad11cb9b9c6b08eafa4a7
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-10-11 14:12:25 +0200

    💄 F04: ORDER > Navbar > Add styles

--- src/Components/pages/order/Navbar.jsx
+++ src/Components/pages/order/Navbar.jsx
@@ -12,11 +12,15 @@ export default function Navbar({ username }) {
 }
 
 const NavbarStyled = styled.nav`
-  background: blue;
   height: 10vh;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  padding-left: 20px;
+  padding-right: 70px;
+
+  border-radius: 15px 15px 0px 0px;
+  background: #fff;
 
   .LeftSide {
     background: pink;
