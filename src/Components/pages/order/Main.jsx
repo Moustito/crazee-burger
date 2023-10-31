7d653f8c2ec67e03f4fc2e4b9b5fab71ec1c2bf1
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-10-31 22:56:39 +0100

    ✨ F05: ORDER > Create <ProductCard /> and add grid

--- src/Components/pages/order/Main.jsx
+++ src/Components/pages/order/Main.jsx
@@ -1,90 +1,25 @@
 import { styled } from "styled-components";
 import { theme } from "../../../theme";
-import burger from "../../../../public/images/burger1.png";
+import ProductCard from "./ProductCard";
 
 export default function Main() {
   return (
     <MainStyled>
-      <div className="card-menu">
-        <img src={burger} alt="" />
-        <div className="card-menu-content">
-          <h3>Titre</h3>
-          <div>
-            <p>Prix</p>
-            <button>Ajouter</button>
-          </div>
-        </div>
-      </div>
+      <ProductCard />
     </MainStyled>
   );
 }
 
 const MainStyled = styled.div`
   flex: 1;
+  display: grid;
+  grid-template-columns: repeat(4, 240px);
+  grid-gap: 85px;
+  justify-content: center;
+
+
   border-radius: 0px 0px ${theme.borderRadius.extraRound}
     ${theme.borderRadius.extraRound};
   background: ${theme.colors.background_white};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
-
-  .card-menu {
-    display: flex;
-    flex-direction: column;
-    border-radius: 15px;
-    background: #fff;
-    box-shadow: -8px 8px 20px 0px rgba(0, 0, 0, 0.2);
-    width: 240px;
-    height: 330px;
-    padding: 50px 20px 25px 20px;
-
-    img {
-      width: 200px;
-    }
-
-    .card-menu-content {
-      display: flex;
-      flex-direction: column;
-      justify-content: flex-end;
-      flex: 1;
-      h3 {
-        color: #17161a;
-        font-family: Amatic SC;
-        font-size: 36px;
-        font-weight: 700;
-      }
-      div {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        p {
-          color: #ffa01b;
-          font-family: Open Sans;
-          font-size: 16px;
-          font-weight: 400;
-        }
-        button {
-          border-radius: 5px;
-          border: 1px solid #ff9f1b;
-          background: #ff9f1b;
-          width: 95px;
-          height: 38px;
-
-          color: #fff;
-          text-align: center;
-          font-family: Arial;
-          font-size: 11px;
-          font-weight: 700;
-
-          &:hover {
-            cursor: pointer;
-            background: #fff;
-            color: #ff9f1b;
-          }
-          &:active {
-            background: #ff9f1b;
-            color: #fff;
-          }
-        }
-      }
-    }
-  }
 `;
