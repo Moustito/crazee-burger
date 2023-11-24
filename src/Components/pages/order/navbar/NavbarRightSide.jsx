90850866aa44a99116fe295cc530c42aa9f01039
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-11-25 00:13:23 +0100

    ✨ F06: ORDER > NAVBAR > Add toast.info

--- src/Components/pages/order/navbar/NavbarRightSide.jsx
+++ src/Components/pages/order/navbar/NavbarRightSide.jsx
@@ -1,25 +1,39 @@
 import { styled } from "styled-components";
+import { useState } from "react";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Profile from "./Profile";
 import ToggleButton from "./ToggleButton";
-import { useState } from "react";
 
 export default function NavbarRightSide({ username }) {
+  const [isChecked, setIsChecked] = useState(false);
 
-    const [isChecked, setIsChecked] = useState(false);
-
-    function handleToggle() {
-      if (isChecked) {
-        console.log("isChecked false");
-        return setIsChecked(false);
-      }
-      console.log("isChecked true");
-      return setIsChecked(true);
+  function handleToggle() {
+    if (isChecked) {
+      return setIsChecked(false);
     }
-
+    toast.info("Mode admin activé", {
+      // icon: <FaUserSecret size={30} />,
+      theme: "dark",
+      position: "bottom-right",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+    return setIsChecked(true);
+  }
 
   return (
     <NavbarRightSideStyled>
-      <ToggleButton isChecked={isChecked} onToggle={() => handleToggle()} labelIfChecked="Désactiver le mode admin" labelIfUnchecked="Activer le mode admin"/>
+      <ToggleButton
+        isChecked={isChecked}
+        onToggle={() => handleToggle()}
+        labelIfChecked="Désactiver le mode admin"
+        labelIfUnchecked="Activer le mode admin"
+      />
       <Profile username={username} />
     </NavbarRightSideStyled>
   );
