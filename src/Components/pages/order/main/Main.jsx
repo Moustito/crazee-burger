b6809c7b7cebb6f78c1818dcdc8ff0e6b8161aa4
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2023-11-26 23:33:40 +0100

    ✨ F07: ORDER > MAIN > Create <AdminPanel />

--- src/Components/pages/order/main/Main.jsx
+++ src/Components/pages/order/main/Main.jsx
@@ -2,12 +2,14 @@ import { styled } from "styled-components";
 import { theme } from "../../../../theme";
 // import Basket from "./Basket";
 import Menu from "./Menu";
+import AdminPanel from "../adminPanel/AdminPanel";
 
 export default function Main() {
   return (
     <MainStyled>
       {/* <Basket /> */}
       <Menu />
+      <AdminPanel />
     </MainStyled>
   );
 }
