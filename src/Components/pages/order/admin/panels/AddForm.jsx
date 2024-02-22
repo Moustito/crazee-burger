f4f5373dba6f10063a43b9dc35e506ce3323cb56
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2024-02-22 02:45:57 +0100

    💄 F08: ADMIN > PANEL > Add style to add panel

--- src/Components/pages/order/admin/panels/AddForm.jsx
+++ src/Components/pages/order/admin/panels/AddForm.jsx
@@ -1,3 +1,87 @@
+import styled from "styled-components";
+import TextInput from "../../../../reusable-ui/TextInput";
+import { FaHamburger } from "react-icons/fa";
+import { BsFillCameraFill } from "react-icons/bs";
+import { MdOutlineEuro } from "react-icons/md";
+
 export default function AddForm() {
-  return <div>AddForm</div>;
+  return (
+    <AddFormStyled>
+      <div className="picture">Aucune image</div>
+      <form action="">
+        <TextInput
+          Icon={<FaHamburger />}
+          placeholder={"Nom du produit (ex: Super Burger)"}
+          className={"AdminForm"}
+        />
+        <TextInput
+          Icon={<BsFillCameraFill />}
+          placeholder={
+            "Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)"
+          }
+          className={"AdminForm"}
+        />
+        <TextInput
+          Icon={<MdOutlineEuro />}
+          placeholder={"Prix"}
+          className={"AdminForm"}
+        />
+        <button>Ajouter un nouveau produit au menu</button>
+      </form>
+    </AddFormStyled>
+  );
 }
+
+const AddFormStyled = styled.div`
+  display: flex;
+  padding-top: 30px;
+  padding-left: 70px;
+
+  .picture {
+    width: 215px;
+    height: 120px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border: 1px solid #e4e5e9;
+    border-radius: 5px;
+    color: #e4e5e9;
+    background-color: white;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+
+    margin-left: 20px;
+
+    div {
+      height: 35px;
+      width: 650px;
+
+      margin: 0;
+      margin-bottom: 8px;
+      padding: 8px, 16px, 8px, 24px;
+
+      background-color: #f5f5f7;
+      color: #747b91;
+    }
+    .AdminForm {
+      margin-left: 10px;
+      background-color: transparent;
+    }
+    button {
+      width: 285px; // 275px
+      height: 35px;
+      padding: 10px 30px;
+
+      border: 1px solid #60bd4f;
+      border-radius: 5px;
+
+      color: white;
+      background-color: #60bd4f;
+    }
+  }
+`;
