47e33fee307d4b3cc46724e3a0d6663f0936787e
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2024-06-19 12:49:11 +0200

    💄 F09: EditForm > Add content and style

--- src/Components/pages/order/admin/panels/EditForm.jsx
+++ src/Components/pages/order/admin/panels/EditForm.jsx
@@ -1,3 +1,34 @@
+import { HiCursorClick } from "react-icons/hi";
+import styled from "styled-components";
+import { theme } from "../../../../../theme";
+
 export default function EditForm() {
-  return <div>EditForm</div>;
+  return (
+    <EditFormStyled>
+      <p>Cliquer sur un produit pour le modifier</p>
+      <HiCursorClick className="icon" />
+    </EditFormStyled>
+  );
 }
+
+const EditFormStyled = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 80px 70px;
+
+  color: ${theme.colors.greyBlue};
+
+  p {
+    font-family: Amatic SC;
+    font-size: ${theme.fonts.size.P3};
+    font-weight: ${theme.fonts.weights.regular};
+    line-height: 30.26px;
+  }
+
+  .icon {
+    width: 24px;
+    height: 24px;
+
+    margin-left: 10px;
+  }
+`;
