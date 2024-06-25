8a6e2ec277bcc287d9add481364cb850c8a5af48
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2024-06-25 23:04:27 +0200

    💄 F10: MAIN > Basket > Add style

--- src/Components/pages/order/main/Basket.jsx
+++ src/Components/pages/order/main/Basket.jsx
@@ -1,14 +1,66 @@
 import styled from "styled-components";
+import { theme } from "../../../../theme";
 
 export default function Basket() {
   return (
     <BasketStyled>
-      <p>Basket</p>
+      <div className="total-price">
+        <p>Total</p>
+        <p>0.00€</p>
+      </div>
+      <div className="panier">
+        <p>Votre commande est vide.</p>
+      </div>
+      <div className="panier-footer">Codé avec ❤️ et React.JS</div>
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
-background-color: purple;
-width: 350px;
+  background-color: purple;
+  width: 350px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 0px 0px 0px 15px;
+
+  .total-price {
+    width: 100%;
+    padding: 12px 16px;
+    display: flex;
+    justify-content: space-between;
+    background: #292729;
+    color: ${theme.colors.primary};
+    font-family: ${theme.fonts.family.stylish};
+    font-size: 36px;
+    font-weight: 400;
+    line-height: 45.4px;
+    letter-spacing: 2px;
+  }
+  .panier {
+    width: 100%;
+    display: flex;
+    flex-grow: 1;
+    align-items: center;
+    justify-content: center;
+    background: #f5f5f7;
+    box-shadow: 0px 0px 20px 0px #00000033 inset;
+    p {
+      font-family: ${theme.fonts.family.stylish};
+      font-size: 36px;
+      font-weight: 400;
+      line-height: 72px;
+      text-align: center;
+    }
+  }
+  .panier-footer {
+    width: 100%;
+    padding: 22px 0;
+    border-radius: 0px 0px 0px 15px;
+    background: #292729;
+    color: white;
+    font-family: ${theme.fonts.family.stylish};
+    text-align: center;
+    font-size: 20px;
+    font-weight: 700;
+  }
 `;
--- src/Components/pages/order/main/Main.jsx
+++ src/Components/pages/order/main/Main.jsx
@@ -5,13 +5,14 @@ import Menu from "./Menu";
 import Admin from "../admin/Admin";
 import { useContext } from "react";
 import OrderContext from "../../../../context/OrderContext";
+import Basket from "./Basket";
 
 export default function Main() {
   const { isModeAdmin } = useContext(OrderContext);
 
   return (
     <MainStyled>
-      {/* <Basket /> */}
+      <Basket />
       <div className="menu-and-admin">
         <Menu />
         {isModeAdmin && <Admin />}
@@ -22,7 +23,8 @@ export default function Main() {
 
 const MainStyled = styled.div`
   height: calc(95vh - 10vh);
-  border-radius: 0px 0px ${theme.borderRadius.extraRound} ${theme.borderRadius.extraRound};
+  border-radius: 0px 0px ${theme.borderRadius.extraRound}
+    ${theme.borderRadius.extraRound};
   background: ${theme.colors.background_white};
   box-shadow: ${theme.shadows.strong};
 
