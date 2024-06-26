48b8b9b84aa693d2f2c7522bca00cd91099b424b
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2024-06-26 13:44:27 +0200

    ♻️ F10: BASKET > Add BasketBody

--- src/Components/pages/order/main/Basket/Basket.jsx
+++ src/Components/pages/order/main/Basket/Basket.jsx
@@ -3,6 +3,7 @@ import { theme } from "../../../../../theme";
 import Total from "./Total";
 import { formatPrice } from "../../../../../utils/maths";
 import Footer from "./Footer";
+import BasketBody from "./BasketBody";
 
 export default function Basket() {
   const testProduct = false;
@@ -10,36 +11,19 @@ export default function Basket() {
   return (
     <BasketStyled>
       <Total amountToPay={formatPrice(0)} />
-      <div className="panier">
-        {testProduct ? <p>Vos produits...</p> : <p>Votre commande est vide.</p>}
-      </div>
+      <BasketBody />
       <Footer />
     </BasketStyled>
   );
 }
 
 const BasketStyled = styled.div`
-  background-color: ${theme.colors.white};
   height: calc(95vh - 10vh);
-
-  border-radius: 0px 0px 0px ${theme.borderRadius.extraRound};
 
   display: flex;
   flex-direction: column;
-  box-shadow: 0px 0px 20px 0px #00000033 inset;
 
-  .panier {
-    width: 100%;
-    display: flex;
-    flex: 1;
-    align-items: center;
-    justify-content: center;
-    p {
-      font-family: ${theme.fonts.family.stylish};
-      font-size: 36px;
-      font-weight: 400;
-      line-height: 72px;
-      text-align: center;
-    }
-  }
+  background-color: ${theme.colors.white};
+
+  border-radius: 0px 0px 0px ${theme.borderRadius.extraRound};
 `;
