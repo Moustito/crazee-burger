1840ccce9e9f1b0d079773e96c08778750ee43a3
Moussiaux Titouan <moussiaux.titouan@gmail.com>
2024-07-05 13:02:56 +0200

    ⚰️ F12: MAIN >Delete UseContext

--- src/Components/pages/order/main/Main.jsx
+++ src/Components/pages/order/main/Main.jsx
@@ -1,13 +1,9 @@
 import { styled } from "styled-components";
 import { theme } from "../../../../theme";
-import { useContext } from "react";
-import OrderContext from "../../../../context/OrderContext";
 import Basket from "./Basket/Basket";
 import MainRightSide from "./MainRightSide/MainRightSide";
 
 export default function Main() {
-  const { isModeAdmin } = useContext(OrderContext);
-
   return (
     <MainStyled>
       <Basket />
